032ecd2ede1b2e2d5f0ce2e65f1430c50635cab2
John Daryl Abkilan <abkilanjohndaryl03@gmail.com>
2025-08-21 22:57:02 +0800

    adding firestore

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAnRYNm0jSJcfUidWXIK94DDGijcC7XfAA",
   authDomain: "library-monitoring-syste-741eb.firebaseapp.com",
   projectId: "library-monitoring-syste-741eb",
-  storageBucket: "library-monitoring-syste-741eb.firebasestorage.app",
+  storageBucket: "library-monitoring-syste-741eb.appspot.com", // ✅ fixed
   messagingSenderId: "13691810191",
   appId: "1:13691810191:web:f61fd1616a411203af9bfa",
   measurementId: "G-QHGVFT5YP0"
 };
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
